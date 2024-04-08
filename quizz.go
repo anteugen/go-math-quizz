8a7a754a4e35654fa0e39ad489cfc61f2e89aa7e
unknown <antoine.eugene7230@gmail.com>
2024-04-08 19:56:16 +0200

    add timer ending the quizz when it is reached

--- quizz.go
+++ quizz.go
@@ -4,6 +4,8 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"flag"
+	"time"
 )
 
 func readProblems() [][]string {
@@ -18,25 +20,58 @@ func readProblems() [][]string {
 	return records
 }
 
+func Timer(timeLimit int) {
+	time.Sleep(time.Duration(timeLimit) * time.Second)
+}
+
+func Score(score int, length int) {
+	fmt.Printf("You finished the test! You scored %v/%v", score, length)
+}
+
 func main() {
+	var timeFlag = flag.Int("limit", 30, "Duration of the quiz")
+	flag.Parse()
+	timeLimit := *timeFlag
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+
+	timerSignal := make(chan bool)
+
+	go func() {
+		<-timer.C
+		timerSignal <- true
+	}()
+
 	records := readProblems()
+	if records == nil {
+		return
+	}
 	length := len(records)
-	fmt.Println(records, length)
 
 	var input string
 	var score int = 0
-
 	for i, v := range records {
 		question := v[0]
-        answer := v[1]
-		fmt.Printf("Problem %v: %v\n", i, question)
-		fmt.Scanln(&input)
-		
-		if input == answer {
-			score += 1
-		}
+		answer := v[1]
+		fmt.Printf("Problem %v: %v = ", i, question)
 
+		answerCh := make(chan string)
+		go func() {
+			var input string
+			fmt.Scanln(&input)
+			answerCh <- input
+		}()
+
+		select {
+		case <-timerSignal:
+			fmt.Println("\nTime's up!")
+			Score(score, length)
+			return
+		case input = <-answerCh:
+			if input == answer {
+				score++
+			}
+		}
 	}
 
-	fmt.Printf("You finished the test! You scored %v/%v", score, length)
+	Score(score, length)
 }
